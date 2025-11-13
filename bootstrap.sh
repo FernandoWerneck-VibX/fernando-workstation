f408d43477f0b2971592227ffcb262cf668bb179
Fernando Werneck <fernando@vibx.com.br>
2025-11-13 01:18:14 -0300

    Minor fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+sudo apt update && sudo apt upgrade -y
+sudo apt dist-upgrade -y
+sudo apt autoremove --purge -y
+
+
 sudo apt update && sudo apt install -y ansible git
 ansible-playbook -i inventory.ini site.yml --ask-become-pass
